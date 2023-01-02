03a34a219e3d51182ec63ff2214872b3b4ee4388
Ekaterina <eka.kachan@gmail.com>
2023-01-02 22:35:11 +0300

    feat: add counter for input in donate page

--- src/donate.js
+++ src/donate.js
@@ -33,3 +33,55 @@ burgerIcon.addEventListener("click", (e) => {
   
 });
 
+const amountRadioButtons = document.querySelectorAll(".amount__radio");
+const anotherAmount = document.querySelector(".feed__another-amount");
+amountRadioButtons.forEach(amountRadio => {
+  amountRadio.addEventListener("click", (e) => {
+    let price = e.target.parentElement.textContent;
+    price = price.slice(price.indexOf("$")+1);
+    price = Number(price)
+    anotherAmount.value = price;
+  });
+});
+anotherAmount.addEventListener("input", (e) => {
+  switch(e.target.value) {
+    case "5000":
+      amountRadioButtons[0].checked = true;
+      break;
+    case "2000":
+      amountRadioButtons[1].checked = true;
+      break;
+    case "1000":
+      amountRadioButtons[2].checked = true;
+      break;
+    case "500":
+      amountRadioButtons[3].checked = true;
+      break;
+    case "250":
+      amountRadioButtons[4].checked = true;
+      break;
+    case "100":
+      amountRadioButtons[5].checked = true;
+      break;
+    case "50":
+      amountRadioButtons[6].checked = true;
+      break;
+    case "25":
+      amountRadioButtons[7].checked = true;
+      break;
+    default:
+      amountRadioButtons.forEach(elem => {
+        elem.checked = false;
+      });
+  }
+});
+
+function validAmount(value) {
+  const regex = /[\d]/;
+  return regex.test(value);
+}
+anotherAmount.addEventListener("keypress", (e) => {
+  if ((! validAmount(e.key)) || (anotherAmount.value.length === 4)) {
+    e.preventDefault();
+  };
+});
